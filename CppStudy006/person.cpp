025af21fb7f2e42b5cb6991e738ec0f2162cf1c3
createjin <createsejin@gmail.com>
2024-02-23 21:27:33 +0900

    2024-02-23-001 transport from RHEL9 to windows VS

--- CppStudy006/person.cpp
+++ CppStudy006/person.cpp
@@ -2,13 +2,14 @@ module person;
 using namespace std;
 namespace person01
 {
-    class Impl {
-        static inline unsigned call_counter{ 0 };
+    //------------------------------------Person::Impl--------------------------------------//
+    class Person::Impl {
         std::string first_name;
         std::string last_name;
         std::string initial;
-        void make_initial();
     public:
+        [[nodiscard]] static auto make_initial(std::string_view first_name,
+            std::string_view last_name) -> std::string;
         Impl();
         ~Impl();
         Impl(const Impl& impl) = default;
@@ -25,183 +26,192 @@ namespace person01
         void set_initial(std::string_view initial);
         void print_info() const;
         void swap(Impl& other) noexcept;
-        bool operator==(const Impl& rhs);
-        bool operator!=(const Impl& rhs);
-        bool operator<(const Impl& rhs);
-        bool operator>=(const Impl& rhs);
-        bool operator>(const Impl& rhs);
-        bool operator<=(const Impl& rhs);
+        bool operator==(const Impl& rhs) const;
+        bool operator!=(const Impl& rhs) const;
+        bool operator<(const Impl& rhs) const;
+        bool operator>=(const Impl& rhs) const;
+        bool operator>(const Impl& rhs) const;
+        bool operator<=(const Impl& rhs) const;
     };
-    void swap(Impl& first, Impl& second) noexcept;
+    //------------------------------------Person::Impl--------------------------------------//
 
-    void Impl::make_initial() {
-        std::string first_initial{ *first_name.begin() };
-        std::string last_initial{ *last_name.begin() };
-        initial = first_initial + last_initial;
+    auto Person::Impl::make_initial(const std::string_view first_name,
+        const std::string_view last_name) -> std::string {
+        const std::string first_initial{ *first_name.begin() };
+        const std::string last_initial{ *last_name.begin() };
+        return first_initial + last_initial;
     }
-    Impl::Impl() {
-        call_counter++;
-        cout << "Calling Default Constructor: " << call_counter << endl;
+    Person::Impl::Impl() {
+        cout << "Calling Impl Default Constructor: " << call_counter++ << " ";
+        print_info();
     }
-    Impl::~Impl() {
-        call_counter++;
-        cout << "Calling Destructor: " << call_counter << endl;
+    Person::Impl::~Impl() {
+        cout << "Calling Impl Destructor: " << call_counter++ << " ";
+        print_info();
     }
 
-    Impl::Impl(const std::string_view first_name, const std::string_view last_name)
-        : Impl(first_name, last_name, "")
+    Person::Impl::Impl(const std::string_view first_name, const std::string_view last_name)
+        : Impl(first_name, last_name, make_initial(first_name, last_name))
     {
-        call_counter++;
-        cout << "Calling parameters 2 Constructor: " << call_counter << endl;
-        make_initial();
+        cout << "Calling Impl parameters 2 Constructor: " << call_counter++ << " ";
+        print_info();
     }
-    Impl::Impl(std::string_view first_name, std::string_view last_name, std::string_view initial)
+    Person::Impl::Impl(const std::string_view first_name, const std::string_view last_name,
+        const std::string_view initial)
         : first_name{ first_name }, last_name{ last_name }, initial{ initial }
     {
-        call_counter++;
-        cout << "Calling parameters 3 Constructor: " << call_counter << endl;
+        cout << "Calling Impl parameters 3 Constructor: " << call_counter++ << " ";
+        print_info();
     }
-    auto Impl::get_first_name() -> std::string& { return first_name; }
+    auto Person::Impl::get_first_name() -> std::string& { return first_name; }
 
-    void Impl::set_first_name(const std::string_view first_name) {
+    void Person::Impl::set_first_name(const std::string_view first_name) {
         this->first_name = first_name;
     }
-    auto Impl::get_last_name() -> std::string& { return last_name; }
-    void Impl::set_last_name(const std::string_view last_name) {
+    auto Person::Impl::get_last_name() -> std::string& { return last_name; }
+    void Person::Impl::set_last_name(const std::string_view last_name) {
         this->last_name = last_name;
     }
-    auto Impl::get_initial() -> std::string& { return initial; }
-    void Impl::set_initial(std::string_view initial) { this->initial = initial; }
+    auto Person::Impl::get_initial() -> std::string& { return initial; }
+    void Person::Impl::set_initial(const std::string_view initial) { this->initial = initial; }
 
-    void Impl::print_info() const {
+    void Person::Impl::print_info() const {
         cout << initial << ": " << first_name << " " << last_name << endl;
     }
 
-    void Impl::swap(Impl& other) noexcept {
+    void Person::Impl::swap(Impl& other) noexcept {
         std::swap(this->first_name, other.first_name);
         std::swap(this->last_name, other.last_name);
         std::swap(this->initial, other.initial);
     }
 
-    bool Impl::operator==(const Impl& rhs)
-    {
+    bool Person::Impl::operator==(const Impl& rhs) const {
         return this->first_name == rhs.first_name &&
             this->last_name == rhs.last_name &&
             this->initial == rhs.initial;
     }
 
-    bool Impl::operator!=(const Impl& rhs)
+    bool Person::Impl::operator!=(const Impl& rhs) const
     {
         return !operator==(rhs);
     }
 
-    bool Impl::operator<(const Impl& rhs)
+    bool Person::Impl::operator<(const Impl& rhs) const
     {
         if (this->first_name != rhs.first_name) return this->first_name < rhs.first_name;
         if (this->last_name != rhs.last_name) return this->last_name < rhs.last_name;
         return this->initial < rhs.initial;
     }
 
-    bool Impl::operator>=(const Impl& rhs)
+    bool Person::Impl::operator>=(const Impl& rhs) const
     {
         return !(*this < rhs);
     }
 
-    bool Impl::operator>(const Impl& rhs)
-    {
+    bool Person::Impl::operator>(const Impl& rhs) const {
         if (this->first_name != rhs.first_name) return this->first_name > rhs.first_name;
         if (this->last_name != rhs.last_name) return this->last_name > rhs.last_name;
         return this->initial > rhs.initial;
     }
 
-    bool Impl::operator<=(const Impl& rhs)
-    {
+    bool Person::Impl::operator<=(const Impl& rhs) const {
         return !(*this > rhs);
     }
 
-    void swap(Impl& first, Impl& second) noexcept {
+    void swap(Person::Impl& first, Person::Impl& second) noexcept {
         first.swap(second);
     }
 
-    Impl& Impl::operator=(Impl&& rhs) noexcept
+    Person::Impl& Person::Impl::operator=(Impl&& rhs) noexcept
     {
         person01::swap(*this, rhs);
         return *this;
     }
 
-    Impl::Impl(Impl&& rhs) noexcept {
+    Person::Impl::Impl(Impl&& rhs) noexcept {
         person01::swap(*this, rhs);
     }
-
-    void Person::make_initial() {
-        impl->make_initial();
+    //------------------------------------Person-------------------------------------------//
+    auto Person::make_initial(const std::string_view first_name,
+        const std::string_view last_name) const -> std::string {
+        return impl->make_initial(first_name, last_name);
     }
     Person::Person() {
         impl = std::make_unique<Impl>();
+        cout << "Calling Person Default Constructor: " << call_counter++ << " ";
+        print_info();
     }
     Person::~Person() {
-        impl->~Impl();
+        cout << "Calling Person Destructor: " << call_counter++ << " ";
+        print_info();
     }
-    
+    Person::Person(const Person& src) { // 복제 생성자
+        *impl = *src.impl;
+    }
+
+    Person& Person::operator=(const Person& rhs) { // 복제 대입 연산자
+        *impl = *rhs.impl;
+        return *this;
+    }
+
     Person::Person(const std::string_view first_name, const std::string_view last_name)
-        : Person(first_name, last_name, "")
+        : Person(first_name, last_name, make_initial(first_name, last_name))
     {
+        cout << "Calling Person parameters 2 Constructor: " << call_counter++ << " ";
+        print_info();
     }
     Person::Person(std::string_view first_name, std::string_view last_name, std::string_view initial)
     {
         impl = std::make_unique<Impl>(first_name, last_name, initial);
+        cout << "Calling Person parameters 3 Constructor: " << call_counter++ << " ";
+        print_info();
     }
-    auto Person::get_first_name() -> std::string& { return impl->get_first_name(); }
+    auto Person::get_first_name() const -> std::string& { return impl->get_first_name(); }
 
-    void Person::set_first_name(const std::string_view first_name) {
+    void Person::set_first_name(const std::string_view first_name) const {
         impl->set_first_name(first_name);
     }
-    auto Person::get_last_name() -> std::string& { return impl->get_last_name(); }
-    void Person::set_last_name(const std::string_view last_name) {
+    auto Person::get_last_name() const -> std::string& { return impl->get_last_name(); }
+    void Person::set_last_name(const std::string_view last_name) const {
         impl->set_last_name(last_name);
     }
-    auto Person::get_initial() -> std::string& { return impl->get_initial(); }
-    void Person::set_initial(std::string_view initial) { impl->set_initial(initial); }
+    auto Person::get_initial() const -> std::string& { return impl->get_initial(); }
+    auto Person::set_initial(const std::string_view initial) const -> void { impl->set_initial(initial); }
 
     void Person::print_info() const {
         impl->print_info();
     }
 
-    void Person::swap(Person& other) noexcept {
-        impl->swap(other);
+    void Person::swap(const Person& other) const noexcept {        
+        impl->swap(*other.impl);
     }
 
-    bool Person::operator==(const Person& rhs)
-    {
-        return (*impl == rhs);
+    bool Person::operator==(const Person& rhs) const {
+        return *impl == *rhs.impl;
     }
 
-    bool Person::operator!=(const Person& rhs)
-    {
-        return !(*impl == rhs);
+    bool Person::operator!=(const Person& rhs) const {
+        return *impl != *rhs.impl;
     }
 
-    bool Person::operator<(const Person& rhs)
-    {        
-        return (*impl < rhs);
+    bool Person::operator<(const Person& rhs) const {
+        return *impl < *rhs.impl;
     }
 
-    bool Person::operator>=(const Person& rhs)
-    {
-        return !(*impl < rhs);
+    bool Person::operator>=(const Person& rhs) const {
+        return *impl >= *rhs.impl;
     }
 
-    bool Person::operator>(const Person& rhs)
-    {
-        return (*impl > rhs);
+    bool Person::operator>(const Person& rhs) const {
+        return *impl > *rhs.impl;
     }
 
-    bool Person::operator<=(const Person& rhs)
-    {
-        return !(*impl > rhs);
+    bool Person::operator<=(const Person& rhs) const {
+        return *impl <= *rhs.impl;
     }
 
-    void swap(Person& first, Person& second) noexcept {
+    // 이걸 어떻게 할까..
+    void swap(const Person& first, const Person& second) noexcept {
         first.swap(second);
     }
 
@@ -215,24 +225,30 @@ namespace person01
         *this->impl = std::move(*rhs.impl);
     }
 
-    static void compare_result(std::weak_ordering result) {
+    [[maybe_unused]]
+    static void compare_result(const std::weak_ordering result) {
         if (is_lt(result)) cout << "less" << endl;
         if (is_gt(result)) cout << "great" << endl;
         if (is_eq(result)) cout << "equal" << endl;
     }
 
-    void study001() { // p.503 연습문제
+    void study001() { // p.503
         auto* person1 = new Person("SeJin"sv, "Bae"sv);
         const auto person3 = std::make_unique<Person>("DeWo"sv, "Lee"sv);
         auto* person4 = new Person("SeAum"sv, "Bae"sv);
         constexpr size_t book_size{ 6 };
-        auto** phoneBook = new Person * [book_size];
+        auto** phoneBook{ new Person * [book_size] };
+        // ReSharper disable once CppDFAMemoryLeak
         phoneBook[0] = new Person("SeHee"sv, "Bae"sv);
         phoneBook[1] = person1;
         phoneBook[2] = person3.get();
+        // ReSharper disable once CppDFAMemoryLeak
         phoneBook[3] = new Person("SeAum"sv, "Kim"sv, "SV"sv);
         phoneBook[4] = person4;
+        // ReSharper disable once CppDFAMemoryLeak
         phoneBook[5] = new Person("SeJin"sv, "Bae"sv, "SV");
+        constexpr int dinamic_array_index[]{ 0, 3, 5 };
+        constexpr size_t dinamic_array_size{ 3 };
         for (size_t i{ 0 }; i < book_size; ++i) {
             phoneBook[i]->print_info();
         }
@@ -241,30 +257,41 @@ namespace person01
         auto result2 = *person1 <=> *phoneBook[3];
         compare_result(result2);
         auto result3 = *phoneBook[3] <=> *phoneBook[4];
-        compare_result(result3);*/
-
-        if (*person4 < *phoneBook[3]) cout << std::format("{0} {1} is less than {2} {3}", 
-            person4->get_first_name(), person4->get_last_name(), 
-            phoneBook[3]->get_first_name(), phoneBook[3]->get_last_name()) << endl;
-        if (*person1 > *person4) cout << std::format("{0} {1} is greater than {2} {3}",
-            person1->get_first_name(), person1->get_last_name(),
-            person4->get_first_name(), person4->get_last_name()) << endl;
-        if (*person1 < *phoneBook[5]) cout << std::format("{0} {1} is less than {2} {3}",
-            person1->get_first_name(), person1->get_last_name(),
-            phoneBook[5]->get_first_name(), phoneBook[5]->get_last_name()) << endl;
-
-        for (size_t i{ 0 }; i < book_size; ++i) {
-            if (i != 2) delete phoneBook[i];
+        compare_result(result3);*/        
+        if (*person4 < *phoneBook[3]) {
+            cout << person4->get_first_name() << " " << person4->get_last_name()
+                << " is less than "
+                << phoneBook[3]->get_first_name() << " " << phoneBook[3]->get_last_name() << endl;
         }
+        if (*person1 > *person4) {
+            cout << person1->get_first_name() << " " << person1->get_last_name()
+                << " is greater than "
+                << person4->get_first_name() << " " << person4->get_last_name() << endl;
+        }
+        if (*person1 < *phoneBook[5]) {
+            cout << person1->get_first_name() << " " << person1->get_last_name()
+                << " is less than "
+                << phoneBook[5]->get_first_name() << " " << phoneBook[5]->get_last_name() << endl;
+        }
+
+        for (size_t i{ 0 }; i < dinamic_array_size; ++i) {
+            delete phoneBook[dinamic_array_index[i]];
+        }
+
+        delete person1;
+        person1 = nullptr;
+        delete person4;
+        person4 = nullptr;
+
         delete[] phoneBook;
         phoneBook = nullptr;
     }
 
     void study002() {
-        auto* person1 = new Person("SeJin"sv, "Bae"sv);
+        const auto* person1 = new Person("SeJin"sv, "Bae"sv);
         person1->print_info();
         delete person1;
-        auto person2 = std::make_unique<Person>("SeHee"sv, "Bae"sv, "UU"sv);
+        const auto person2 = std::make_unique<Person>("SeHee"sv, "Bae"sv, "UU"sv);
         person2->print_info();
     }
 }
--- CppStudy006/person.ixx
+++ CppStudy006/person.ixx
@@ -3,40 +3,43 @@ import std;
 
 namespace person01
 {
+    unsigned call_counter{ 0 };
+
     export class Person
     {
-        static inline unsigned call_counter{ 0 };
         std::string first_name;
         std::string last_name;
         std::string initial;
-        void make_initial();
+        [[nodiscard]] std::string make_initial(std::string_view first_name, std::string_view last_name) const;
         class Impl;
         std::unique_ptr<Impl> impl;
+        friend void swap(Impl& first, Impl& second) noexcept;
     public:
         Person();
         ~Person();
-        Person(const Person& person) = default;
-        Person& operator=(const Person& rhs) = default;
+        Person(const Person& src);
+        Person& operator=(const Person& rhs);
         Person(Person&& rhs) noexcept;
         Person& operator=(Person&& rhs) noexcept;
         Person(std::string_view first_name, std::string_view last_name);
         Person(std::string_view first_name, std::string_view last_name, std::string_view initial);
-        [[nodiscard]] auto get_first_name() -> std::string&;
-        void set_first_name(std::string_view first_name);
-        [[nodiscard]] auto get_last_name() -> std::string&;
-        void set_last_name(std::string_view last_name);
-        [[nodiscard]] auto get_initial() -> std::string&;
-        void set_initial(std::string_view initial);
+        [[nodiscard]] auto get_first_name() const->std::string&;
+        void set_first_name(std::string_view first_name) const;
+        [[nodiscard]] auto get_last_name() const->std::string&;
+        void set_last_name(std::string_view last_name) const;
+        [[nodiscard]] auto get_initial() const->std::string&;
+        void set_initial(std::string_view initial) const;
         void print_info() const;
-        void swap(Person& other) noexcept;
+        void swap(const Person& other) const noexcept;
         //[[nodiscard]] std::weak_ordering operator<=>(const Person& rhs) const = default;
-        bool operator==(const Person& rhs);
-        bool operator!=(const Person& rhs);
-        bool operator<(const Person& rhs);
-        bool operator>=(const Person& rhs);
-        bool operator>(const Person& rhs);
-        bool operator<=(const Person& rhs);
+        bool operator==(const Person& rhs) const;
+        bool operator!=(const Person& rhs) const;
+        bool operator<(const Person& rhs) const;
+        bool operator>=(const Person& rhs) const;
+        bool operator>(const Person& rhs) const;
+        bool operator<=(const Person& rhs) const;
     };
+
     void swap(Person& first, Person& second) noexcept;
 
     export void study001();
